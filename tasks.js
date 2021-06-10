8471c98a4d5beef736d7594c3186f6b55ad9bd88
Dana Allam <dana.allam99@hotmail.com>
2021-06-10 23:50:09 +0300

    help command

--- tasks.js
+++ tasks.js
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+    help();
+  }
   else{
     unknownCommand(text);
   }
@@ -66,6 +69,11 @@ function unknownCommand(c){
 function hello(){
   console.log('hello!')
 }
+
+function help(){
+  console.log('Options\n -hello\n -quit\n -exit')
+}
+
 
 
 /**
